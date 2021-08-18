9ea1c8214d142543d1a85e3f1a8dd6613d25383b
Sandesh Chapagain <chapagainsandesh88@gmail.com>
2021-08-18 17:03:35 -0400

    get pov switch working

--- src/Test.js
+++ src/Test.js
@@ -3,43 +3,57 @@ import * as THREE from "three";
 import * as CANNON from "cannon";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import Loading from "./Loading";
-import Matter from "matter-js";
+import { Vector3 } from "three";
 
 var world,
+  controls,
   ball,
   earth,
   rod,
-  shape,
   timeStep = 1 / 60,
   camera,
+  mainCamera,
+  earthCamera,
   scene,
   renderer,
-  geometry,
-  material,
   ballMesh,
   earthMesh,
-  engine,
+  cameraTarget,
+  currentView = "main",
+  cameraHelper,
   rodMesh;
 
-const ROD_RADIUS = 0.1;
 const BALL_RADIUS = 1;
-const ROD_HEIGHT = 10;
 const EARTH_RADIUS = 10;
-const EARTH_MASS = 100;
-const SPEEDUP_FACTOR = 8640;
-const MAX_ANGLE = Math.PI / 4;
-
-// var Engine = Matter.Engine,
-//         Render = Matter.Render,
-//         Runner = Matter.Runner,
-//         Composites = Matter.Composites,
-//         MouseConstraint = Matter.MouseConstraint,
-//         Mouse = Matter.Mouse,
-//         Composite = Matter.Composite,
-//         Constraint = Matter.Constraint,
-//         Bodies = Matter.Bodies;
+const GRAVITY_ACCN = -9.81;
 
 const loader = new THREE.TextureLoader();
+
+const degToRad = (angle) => (angle * Math.PI) / 180;
+
+const getMainCamera = (aspect, target) => {
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    1,
+    100
+  );
+  camera.position.z = EARTH_RADIUS * 3;
+  camera.aspect = aspect;
+  return camera;
+};
+
+const getEarthCamera = (target) => {
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    1,
+    100
+  );
+  camera.translateY(EARTH_RADIUS);
+  camera.translateX(1);
+  return camera;
+};
 
 export default function Test() {
   const [container, setContainer] = useState();
@@ -51,29 +65,8 @@ export default function Test() {
     }
   };
 
-  let controls;
-
-  // function initMatter() {
-  //   engine = Engine.create();
-  //   world= engine.world;
-
-  //   const runner = Runner.create();
-  //   Runner.run(runner,engine);
-
-  //   ball = Bodies.
-  // }
-
   function initThree(wrapper) {
     scene = new THREE.Scene();
-    camera = new THREE.PerspectiveCamera(
-      75,
-      window.innerWidth / window.innerHeight,
-      1,
-      100
-    );
-    camera.position.z = EARTH_RADIUS * 3;
-    camera.aspect = wrapper.offsetWidth / wrapper.offsetHeight;
-    camera.updateProjectionMatrix();
 
     renderer = new THREE.WebGLRenderer({ antialias: true });
     renderer.setSize(wrapper.offsetWidth, wrapper.offsetHeight);
@@ -83,25 +76,15 @@ export default function Test() {
       color: 0x00ffff,
     });
 
-    controls = new OrbitControls(camera, container);
-
     const pendulum = new THREE.Group();
     scene.add(pendulum);
-    pendulum.position.y = EARTH_RADIUS * 2;
     ballMesh = new THREE.Mesh(ballGeometry, material);
-
-    // const rodGeometry = new THREE.CylinderGeometry(
-    //   ROD_RADIUS,
-    //   ROD_RADIUS,
-    //   10,
-    //   32,
-    //   32
-    // );
-    const rodGeometry = new THREE.SphereGeometry(BALL_RADIUS, 32, 16);
-    rodMesh = new THREE.Mesh(rodGeometry, material);
-    rodMesh.position.y = 0.5 * ROD_HEIGHT;
-    pendulum.add(rodMesh);
-
+    const pivot = new THREE.Mesh(
+      new THREE.SphereGeometry(0.1, 32, 16),
+      material
+    );
+    pivot.position.set(0, 20, 0);
+    pendulum.add(pivot);
     pendulum.add(ballMesh);
 
     const earthGeometry = new THREE.SphereGeometry(EARTH_RADIUS, 64, 32);
@@ -110,12 +93,28 @@ export default function Test() {
     });
 
     earthMesh = new THREE.Group();
+    earthMesh.position.y = -EARTH_RADIUS;
     scene.add(earthMesh);
-    scene.add(camera);
 
     const _earthMesh = new THREE.Mesh(earthGeometry, earthMaterial);
+    _earthMesh.rotateZ(degToRad(-23));
     earthMesh.add(_earthMesh);
     wrapper.appendChild(renderer.domElement);
+
+    const aspect = wrapper.offsetWidth / wrapper.offsetHeight;
+    mainCamera = getMainCamera(aspect, new Vector3(0, 0, 0));
+    earthCamera = getEarthCamera(new Vector3(0, 20, 0));
+    camera = mainCamera;
+    cameraTarget = new Vector3(0, 0, 0);
+
+    cameraHelper = new THREE.CameraHelper(earthCamera);
+    earthCamera.lookAt(new Vector3(0, 30, 0));
+    scene.add(cameraHelper);
+
+    scene.add(mainCamera);
+    earthMesh.add(earthCamera);
+
+    // controls = new OrbitControls(camera, wrapper);
 
     const onWindowResize = () => {
       if (!container) return;
@@ -128,51 +127,36 @@ export default function Test() {
 
   function initCannon() {
     world = new CANNON.World();
-    world.gravity.set(0, 0, 0);
+    world.gravity.set(0, GRAVITY_ACCN, 0);
     world.broadphase = new CANNON.NaiveBroadphase();
     world.solver.iterations = 10;
     world.quatNormalizeFast = false;
     world.quatNormalizeSkip = 0;
 
-    // const rodShape = new CANNON.Cylinder(
-    //   ROD_RADIUS,
-    //   ROD_RADIUS,
-    //   ROD_HEIGHT,
-    //   100
-    // );
-    const rodShape = new CANNON.Sphere(BALL_RADIUS);
-    rod = new CANNON.Body({
+    const pivotShape = new CANNON.Sphere(1);
+    const pivot = new CANNON.Body({
       mass: 0,
     });
-
-    rod.addShape(rodShape);
-    world.addBody(rod);
-    rod.position.set(0, 0, 7 * BALL_RADIUS);
+    pivot.addShape(pivotShape);
+    pivot.position.set(0, 20, 0);
+    world.addBody(pivot);
 
     const ballShape = new CANNON.Sphere(BALL_RADIUS);
     ball = new CANNON.Body({
-      mass: EARTH_MASS,
+      mass: 100,
     });
-    ball.position.set(0, 0, -BALL_RADIUS * 3);
-    ball.velocity.set(5, 0, 0);
+
+    ball.velocity.set(-5, 0, 0);
+    ball.position.set(0, 10, 0);
     ball.linearDamping = 0;
     ball.angularDamping = 0;
     ball.addShape(ballShape);
     world.addBody(ball);
 
-    // const earthShape = new CANNON.Sphere(EARTH_RADIUS);
-
-    // earth = new CANNON.Body({
-    //   mass: EARTH_MASS,
-    // });
-    // earth.addShape(earthShape);
-    // earth.angularVelocity.set(0, (SPEEDUP_FACTOR * (2 * Math.PI)) / 86400, 0);
-    // world.addBody(earth);
-
     var c = new CANNON.PointToPointConstraint(
       ball,
-      rod.position,
-      rod,
+      pivot.position,
+      pivot,
       ball.position
     );
     world.addConstraint(c);
@@ -180,9 +164,13 @@ export default function Test() {
 
   function animate() {
     renderer.render(scene, camera);
-    controls.update();
+    camera.lookAt(cameraTarget);
+    camera.updateProjectionMatrix();
+
     updatePhysics();
     requestAnimationFrame(animate);
+    // earthMesh.rotateOnAxis(new THREE.Vector3(0.39, 0.92, 0), 0.05);
+    earthMesh.rotateOnAxis(new THREE.Vector3(0, 1, 0), 0.05);
   }
 
   function updatePhysics() {
@@ -210,10 +198,28 @@ export default function Test() {
     setLoading(false);
   };
 
+  function toggleCameraView() {
+    if (camera === mainCamera) {
+      console.log("switching to earth view");
+      camera = earthCamera;
+      cameraTarget = new Vector3(0, 20, 0);
+    } else {
+      console.log("switching to main view");
+      camera = mainCamera;
+      cameraTarget = new Vector3(0, 0, 0);
+    }
+  }
+
   return (
     <>
       {isLoading && <Loading />}
       <div className="w-full h-full" ref={containerMountCb}></div>
+      <button
+        onClick={toggleCameraView}
+        className="fixed z-50 px-3 py-2 top-5 left-5 rounded-2xl bg-indigo-300"
+      >
+        Switch!
+      </button>
     </>
   );
 }
