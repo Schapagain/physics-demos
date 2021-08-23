d7bf6394d9acee2f2701c1fd9b8421381c46a2d3
Sandesh Chapagain <chapagainsandesh88@gmail.com>
2021-08-23 12:08:05 -0400

    separate worlds, and fix periods

--- src/Test.js
+++ src/Test.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from "react";
 import * as THREE from "three";
-import * as CANNON from "cannon";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import Loading from "./Loading";
 import { Vector3 } from "three";
+import * as CANNON from "cannon";
 
 var world,
+  world2,
   controls,
   ball,
   earth,
@@ -18,6 +19,7 @@ var world,
   renderer,
   ballMesh,
   earthMesh,
+  rodMesh,
   cameraTarget,
   currentView = "main",
   cameraHelper,
@@ -26,17 +28,22 @@ var world,
 const BALL_RADIUS = 1;
 const EARTH_RADIUS = 10;
 const GRAVITY_ACCN = -9.81;
+const DEMO_SPEED = 10000;
+const SECONDS_IN_DAY = 86400;
+const PENDULUM_TO_SECONDS_IN_DAY = 1;
+const DISPLAY_PENDULUM_LENGTH = 10;
 
 const loader = new THREE.TextureLoader();
 
 const degToRad = (angle) => (angle * Math.PI) / 180;
+const radToDeg = (angle) => (angle * 180) / Math.PI;
 
 const getMainCamera = (aspect, target) => {
   const camera = new THREE.PerspectiveCamera(
     75,
     window.innerWidth / window.innerHeight,
     1,
-    100
+    200
   );
   camera.position.z = EARTH_RADIUS * 3;
   camera.aspect = aspect;
@@ -48,16 +55,44 @@ const getEarthCamera = (target) => {
     75,
     window.innerWidth / window.innerHeight,
     1,
-    100
+    200
   );
   camera.translateY(EARTH_RADIUS);
   camera.translateX(1);
   return camera;
 };
 
+function periodToLength(period) {
+  return (period / (2 * Math.PI)) ** 2 * Math.abs(GRAVITY_ACCN);
+}
+
+function rotateAboutPoint(obj, point, axis, theta, pointIsWorld) {
+  pointIsWorld = pointIsWorld === undefined ? false : pointIsWorld;
+
+  if (pointIsWorld) {
+    obj.parent.localToWorld(obj.position); // compensate for world coordinate
+  }
+
+  obj.position.sub(point); // remove the offset
+  obj.position.applyAxisAngle(axis, theta); // rotate the POSITION
+  obj.position.add(point); // re-add the offset
+
+  if (pointIsWorld) {
+    obj.parent.worldToLocal(obj.position); // undo world coordinates compensation
+  }
+
+  obj.rotateOnAxis(axis, theta); // rotate the OBJECT
+}
+
 export default function Test() {
   const [container, setContainer] = useState();
   const [isLoading, setLoading] = useState(true);
+  const [pendulumObj, setPendulumObj] = useState({
+    period: (PENDULUM_TO_SECONDS_IN_DAY * SECONDS_IN_DAY) / DEMO_SPEED,
+    mass: 1,
+    intialAngle: Math.PI / 4,
+  });
+  const [earthPeriod, setEarthPeriod] = useState(SECONDS_IN_DAY / DEMO_SPEED);
 
   const containerMountCb = (node) => {
     if (node) {
@@ -79,13 +114,15 @@ export default function Test() {
     const pendulum = new THREE.Group();
     scene.add(pendulum);
     ballMesh = new THREE.Mesh(ballGeometry, material);
+    ballMesh.position.set(
+      periodToLength(pendulumObj.period) * Math.sin(pendulumObj.intialAngle),
+      -periodToLength(pendulumObj.period) * Math.cos(pendulumObj.intialAngle),
+      0
+    );
     const pivot = new THREE.Mesh(
       new THREE.SphereGeometry(0.1, 32, 16),
       material
     );
-    pivot.position.set(0, 20, 0);
-    pendulum.add(pivot);
-    pendulum.add(ballMesh);
 
     const earthGeometry = new THREE.SphereGeometry(EARTH_RADIUS, 64, 32);
     const earthMaterial = new THREE.MeshBasicMaterial({
@@ -93,28 +130,44 @@ export default function Test() {
     });
 
     earthMesh = new THREE.Group();
-    earthMesh.position.y = -EARTH_RADIUS;
+    earthMesh.position.y = -EARTH_RADIUS - 3 * DISPLAY_PENDULUM_LENGTH;
     scene.add(earthMesh);
 
     const _earthMesh = new THREE.Mesh(earthGeometry, earthMaterial);
-    _earthMesh.rotateZ(degToRad(-23));
     earthMesh.add(_earthMesh);
+
+    pendulum.add(pivot);
+    pendulum.add(ballMesh);
+
+    const rodGeometry = new THREE.CylinderGeometry(
+      0.05,
+      0.05,
+      DISPLAY_PENDULUM_LENGTH,
+      32,
+      16
+    );
+    rodMesh = new THREE.Mesh(rodGeometry, material);
+    rodMesh.translateY(-DISPLAY_PENDULUM_LENGTH / 2);
+    pendulum.add(rodMesh);
+
     wrapper.appendChild(renderer.domElement);
 
     const aspect = wrapper.offsetWidth / wrapper.offsetHeight;
-    mainCamera = getMainCamera(aspect, new Vector3(0, 0, 0));
-    earthCamera = getEarthCamera(new Vector3(0, 20, 0));
+    mainCamera = getMainCamera(aspect);
+    earthCamera = getEarthCamera();
     camera = mainCamera;
     cameraTarget = new Vector3(0, 0, 0);
 
-    cameraHelper = new THREE.CameraHelper(earthCamera);
-    earthCamera.lookAt(new Vector3(0, 30, 0));
-    scene.add(cameraHelper);
+    // cameraHelper = new THREE.CameraHelper(earthCamera);
+    // earthCamera.lookAt(new Vector3(0, 0, 0));
+    // scene.add(cameraHelper);
 
     scene.add(mainCamera);
     earthMesh.add(earthCamera);
 
-    // controls = new OrbitControls(camera, wrapper);
+    controls = new OrbitControls(camera, wrapper);
+    controls.maxDistance = 80;
+    controls.minDistance = 20;
 
     const onWindowResize = () => {
       if (!container) return;
@@ -126,6 +179,11 @@ export default function Test() {
   }
 
   function initCannon() {
+    initPendulumPhysics(pendulumObj);
+  }
+
+  function initPendulumPhysics(pendulumObj) {
+    console.log(pendulumObj);
     world = new CANNON.World();
     world.gravity.set(0, GRAVITY_ACCN, 0);
     world.broadphase = new CANNON.NaiveBroadphase();
@@ -133,65 +191,106 @@ export default function Test() {
     world.quatNormalizeFast = false;
     world.quatNormalizeSkip = 0;
 
+    world2 = new CANNON.World();
+    world2.gravity.set(0, 0, 0);
+    world2.broadphase = new CANNON.NaiveBroadphase();
+    world2.solver.iterations = 10;
+    world2.quatNormalizeFast = false;
+    world2.quatNormalizeSkip = 0;
+
     const pivotShape = new CANNON.Sphere(1);
     const pivot = new CANNON.Body({
       mass: 0,
     });
     pivot.addShape(pivotShape);
-    pivot.position.set(0, 20, 0);
+
     world.addBody(pivot);
 
     const ballShape = new CANNON.Sphere(BALL_RADIUS);
     ball = new CANNON.Body({
-      mass: 100,
+      mass: 1,
     });
 
-    ball.velocity.set(-5, 0, 0);
-    ball.position.set(0, 10, 0);
+    ball.position.set(
+      periodToLength(pendulumObj.period) * Math.sin(pendulumObj.intialAngle),
+      -periodToLength(pendulumObj.period) * Math.cos(pendulumObj.intialAngle),
+      0
+    );
     ball.linearDamping = 0;
     ball.angularDamping = 0;
     ball.addShape(ballShape);
     world.addBody(ball);
 
-    var c = new CANNON.PointToPointConstraint(
+    const earthShape = new CANNON.Sphere(EARTH_RADIUS);
+
+    earth = new CANNON.Body({
+      mass: 100,
+    });
+    earth.addShape(earthShape);
+    earth.angularDamping = 0;
+    earth.angularVelocity.set(0, (2 * Math.PI) / earthPeriod, 0);
+    console.log(earth.angularVelocity);
+    world2.addBody(earth);
+
+    var c = new CANNON.DistanceConstraint(
       ball,
-      pivot.position,
       pivot,
-      ball.position
+      periodToLength(pendulumObj.period)
     );
     world.addConstraint(c);
   }
-
+  var prevAngle = 0;
   function animate() {
+    updatePhysics();
     renderer.render(scene, camera);
     camera.lookAt(cameraTarget);
     camera.updateProjectionMatrix();
+    controls.update();
 
-    updatePhysics();
     requestAnimationFrame(animate);
-    earthMesh.rotateOnAxis(new THREE.Vector3(0, 1, 0), 0.006);
+    const currAngle = Math.atan(-ballMesh.position.x / ballMesh.position.y);
+    rotateAboutPoint(
+      rodMesh,
+      new Vector3(0, 0, 0),
+      new Vector3(0, 0, 1),
+      currAngle - prevAngle
+    );
+    prevAngle = currAngle;
   }
 
   function updatePhysics() {
     // Step the physics world
     world.step(timeStep);
+    world2.step(timeStep);
 
-    // Copy coordinates from Cannon.js to Three.js
-    ball & ballMesh.position.copy(ball.position);
-    ball & ballMesh.quaternion.copy(ball.quaternion);
-    rod && rodMesh.position.copy(rod.position);
-    rod && rodMesh.quaternion.copy(rod.quaternion);
-    earth && earthMesh.position.copy(earth.position);
+    // Scale and copy coordinates from Cannon.js to Three.js
+    if (ball) {
+      const ballX = ball.position.x;
+      const ballY = ball.position.y;
+      const ballZ = ball.position.z;
+
+      const theta = Math.atan(ballX / ballY);
+      const scaledX = DISPLAY_PENDULUM_LENGTH * Math.sin(theta);
+      const scaledY = -DISPLAY_PENDULUM_LENGTH * Math.cos(theta);
+      const scaledZ = ballZ;
+      ballMesh.position.copy(new Vector3(scaledX, scaledY, scaledZ));
+    }
     earth && earthMesh.quaternion.copy(earth.quaternion);
   }
-
   useEffect(() => {
     if (container) {
-      initThree(container);
       initCannon(container);
+      initThree(container);
+
       animate();
     }
   }, [container]);
+
+  useEffect(() => {
+    if (pendulumObj && world) {
+      initPendulumPhysics(pendulumObj);
+    }
+  }, [pendulumObj]);
 
   THREE.DefaultLoadingManager.onLoad = function () {
     setLoading(false);
@@ -201,24 +300,54 @@ export default function Test() {
     if (camera === mainCamera) {
       console.log("switching to earth view");
       camera = earthCamera;
-      cameraTarget = new Vector3(0, 20, 0);
+      controls.enabled = false;
     } else {
       console.log("switching to main view");
       camera = mainCamera;
-      cameraTarget = new Vector3(0, 0, 0);
+      controls.enabled = true;
     }
   }
+
+  const updatePendulum = (prop, val) => {
+    setPendulumObj((obj) => ({ ...obj, [prop]: val }));
+  };
 
   return (
     <>
       {isLoading && <Loading />}
       <div className="w-full h-full" ref={containerMountCb}></div>
-      <button
-        onClick={toggleCameraView}
-        className="fixed z-30 px-3 py-2 top-5 left-5 rounded-2xl bg-indigo-300"
-      >
-        Switch!
-      </button>
+      <div className="fixed z-30 flex flex-col top-5 left-5">
+        <button
+          onClick={toggleCameraView}
+          className="px-3 py-2 rounded-2xl bg-indigo-300"
+        >
+          Switch!
+        </button>
+        <span className="text-white">
+          Pendulum period: {pendulumObj.period / earthPeriod} x Earth's period
+        </span>
+        <input
+          type="range"
+          max={80}
+          min={0}
+          step={10}
+          value={radToDeg(pendulumObj.intialAngle)}
+          onChange={(e) =>
+            updatePendulum("initialAngle", degToRad(Number(e.target.value)))
+          }
+        />
+        <span className="text-white">Demo Speed: {DEMO_SPEED}</span>
+        <input
+          type="range"
+          max={80}
+          min={0}
+          step={10}
+          value={radToDeg(pendulumObj.intialAngle)}
+          onChange={(e) =>
+            updatePendulum("initialAngle", degToRad(Number(e.target.value)))
+          }
+        />
+      </div>
     </>
   );
 }
